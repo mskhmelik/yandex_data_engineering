418c506ec6d5ccf258822821572608b1b4cacbea
mskhmelik <michael.khmelik@gmail.com>
2025-08-01 15:29:44 +0100

    add MERGE statement to update records in dwh.f_order

--- Project_1/project_1.sql
+++ Project_1/project_1.sql
@@ -214,3 +214,37 @@ JOIN dwh.d_product dp
     ON dp.product_name = src.product_name 
    AND dp.product_description = src.product_description 
    AND dp.product_price = src.product_price;
+
+/* 6. Update all records and load new records в dwh.f_order */
+MERGE INTO dwh.f_order f
+USING df_temp_fact t
+ON f.product_id = t.product_id 
+   AND f.craftsman_id = t.craftsman_id 
+   AND f.customer_id = t.customer_id 
+   AND f.order_created_date = t.order_created_date
+
+WHEN MATCHED THEN
+    UPDATE SET 
+        order_completion_date = t.order_completion_date,
+        order_status = t.order_status,
+        load_dttm = current_timestamp
+
+WHEN NOT MATCHED THEN
+    INSERT (
+        product_id, 
+        craftsman_id, 
+        customer_id, 
+        order_created_date, 
+        order_completion_date, 
+        order_status, 
+        load_dttm
+    )
+    VALUES (
+        t.product_id, 
+        t.craftsman_id, 
+        t.customer_id, 
+        t.order_created_date, 
+        t.order_completion_date, 
+        t.order_status, 
+        current_timestamp
+    );
